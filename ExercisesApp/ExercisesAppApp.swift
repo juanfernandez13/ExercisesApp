582cc53a9b7a48d400d424c50def7e13028a65ab
juanfernandez <juanp1310@outlook.com>
2025-03-20 14:33:15 -0300

    Initial Commit

---
+++ ExercisesApp/ExercisesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExercisesAppApp.swift
+//  ExercisesApp
+//
+//  Created by user on 20/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ExercisesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
