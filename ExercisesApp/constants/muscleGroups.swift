bc073ccae3c48aa0fd8cc271a1bf982ab6c80eed
Juan Fernandez <juanp1310@outlook.com>
2025-03-21 17:09:14 -0300

    feat - exercises model

---
+++ ExercisesApp/constants/muscleGroups.swift
@@ -0,0 +1,132 @@
+//
+//  muscleGroups.swift
+//  ExercisesApp
+//
+//  Created by user on 21/03/25.
+
+
+var muscleGroups: [String: [String]] = [
+    "Peito": [
+        "Supino reto",
+        "Supino inclinado",
+        "Peck Deck",
+        "Crucifixo",
+        "Flexão de braço",
+        "Supino com halteres",
+        "Pullover",
+        "Crossover",
+        "Supino declinado"
+    ],
+    "Dorsal": [
+        "Puxada frente na barra",
+        "Remada curvada",
+        "Pull-up",
+        "Remada com halteres",
+        "Puxada no pulley",
+        "Remada unilateral",
+        "Levantamento terra",
+        "Remada com barra T",
+        "Puxada atrás da cabeça"
+    ],
+    "Trapézio": [
+        "Encolhimento de ombros com barra",
+        "Encolhimento de ombros com halteres",
+        "Face Pull",
+        "Reverse Fly",
+        "Elevação vertical",
+        "High Pull",
+        "Arnold Press"
+    ],
+    "Lombar": [
+        "Levantamento terra",
+        "Bom dia",
+        "Hiperextensão",
+        "Stiff",
+        "Balancinho com kettlebell",
+        "Levantamento terra romeno",
+        "Superman",
+        "Elevação de quadril",
+        "Extensão de costas"
+    ],
+    "Ombro": [
+        "Desenvolvimento militar",
+        "Elevação lateral",
+        "Elevação frontal",
+        "Arnold Press",
+        "Desenvolvimento com halteres",
+        "Face Pull",
+        "Push Press",
+        "Elevação lateral na polia"
+    ],
+    "Quadríceps": [
+        "Agachamento",
+        "Leg Press",
+        "Cadeira extensora",
+        "Agachamento frontal",
+        "Afundo",
+        "Step-up",
+        "Agachamento sumô",
+        "Agachamento búlgaro",
+        "Hack Squat"
+    ],
+    "Posterior de coxa": [
+        "Stiff",
+        "Cadeira flexora",
+        "Levantamento terra com pernas rígidas",
+        "Elevação de glúteos e posterior",
+        "Elevação de quadril",
+        "Bom dia",
+        "Flexão de pernas",
+        "Levantamento terra romeno"
+    ],
+    "Glúteos": [
+        "Elevação de quadril",
+        "Ponte de glúteos",
+        "Agachamento",
+        "Levantamento terra",
+        "Balancinho com kettlebell",
+        "Step-up",
+        "Agachamento búlgaro",
+        "Cadeira abdutora",
+        "Afundo"
+    ],
+    "Tríceps": [
+        "Tríceps na polia",
+        "Supino fechado",
+        "Mergulho",
+        "Tríceps testa",
+        "Tríceps com halteres",
+        "Skull Crushers",
+        "Kickback",
+        "Tríceps na corda"
+    ],
+    "Bíceps": [
+        "Rosca direta",
+        "Rosca alternada",
+        "Rosca martelo",
+        "Rosca concentrada",
+        "Rosca Scott",
+        "Rosca na polia baixa",
+        "Rosca com barra",
+        "Rosca no banco Scott"
+    ],
+    "Panturrilha": [
+        "Elevação de panturrilha em pé",
+        "Elevação de panturrilha sentado",
+        "Leg Press para panturrilha",
+        "Panturrilha na máquina",
+        "Panturrilha no Smith Machine",
+        "Elevação de panturrilha burro"
+    ],
+    "Abdômen": [
+        "Prancha",
+        "Crunch",
+        "Ab roll-out",
+        "Elevação de pernas",
+        "Russian twist",
+        "Bicicleta no chão",
+        "Elevação de pernas suspenso",
+        "Mountain Climbers"
+    ]
+]
+
